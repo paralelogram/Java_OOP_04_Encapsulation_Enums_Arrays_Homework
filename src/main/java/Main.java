a40b948fec578b12b1904ab9525a2df7489e5e8a
paralelogram <sabiy1999@gmail.com>
2017-11-05 21:36:04 +0200

    second task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,50 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String [] args) {
-        String country;
-        Continents continent;
-        System.out.println("in which country do you live?");
+    public static void main (String [] args) {
+        int [] array = new int[10];
+        int max = array[0];
+        int sum = 0;
+        int amount = 0;
+        boolean bool = false;
+
         Scanner scanner = new Scanner(System.in);
-        country = scanner.nextLine();
+        System.out.println("input 10 integer digits");
 
-        switch (country) {
-            case "Algeria" : case "Kenya" : case "Uganda" : case "Zambia" : case "Zimbabwe" : case "Egypt" :
-                continent = Continents.AFRICA;
-                System.out.println("your country is in Africa"); break;
-            case "Taiwan" : case "Thailand" : case "Afghanistan" : case "Nepal" : case "North Korea" : case "Uzbekistan" :
-                continent = Continents.ASIA;
-                System.out.println("your country is in Asia"); break;
-            case "Italy" : case "San Marino" : case "Poland" : case "Turkey" : case "Latvia" : case "Ukraine" :
-                continent = Continents.EUROPE;
-                System.out.println("your country is in Europe"); break;
-            case "Grenada" : case "Mexico" : case "Panama" : case "Cuba" : case "Dominica" : case "Haiti" :
-                continent = Continents.NORTH_AMERICA;
-                System.out.println("your country is in North America"); break;
-            case "Argentina" : case "Paraguay" : case "Suriname" : case "Venezuela" : case "Colombia" : case "Ecuador" :
-                continent = Continents.SOUTH_AMERICA;
-                System.out.println("your country is in South America");break;
-            case "Australia" : case "Palau" : case "Tonga" : case "Tuvalu" : case "Vanuatu" : case "Samoa" :
-                continent = Continents.AUSTRALIA_AND_OCEANIA;
-                System.out.println("your country in Australia, or in Oceania");break;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
         }
-    }
-}
 
-enum Continents {
-    NORTH_AMERICA, SOUTH_AMERICA, ASIA, EUROPE, AFRICA, AUSTRALIA_AND_OCEANIA;
+        int p = 0;
+        int n = 0;
+        for (int i = 0; i < array.length; i++) {
+            if ( array[i] > max) {
+                max = array[i];
+            }
+
+            if (array[i] > 0) {
+                sum += array[i];
+                p++;
+            }
+
+            if (array[i] < 0) {
+                n++;
+            }
+
+            if (array[i] != 0){
+                amount += array[i];
+            }
+
+            if (n < p) {
+                bool = true;
+            }
+            if (n > p) {
+                bool = false;
+            }
+        }
+        System.out.println("the biggest of numbers = " + max);
+        System.out.println("the sum of positive numbers in the array = " + sum);
+        System.out.println("the amount of negative and positive numbers in the array = " + amount);
+        System.out.println("are positive ones more than negative? \n" + bool);
+    }
 }
