48f2841fe5f44df7af22a90b304a1f6d8f7ca69f
paralelogram <sabiy1999@gmail.com>
2017-11-07 14:37:33 +0200

    third task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,95 @@
 import java.util.Scanner;
 
-public class Main {
+public class Employee {
+    private String name;
+    private Departments department;
+    private double salary;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDepartment(Departments department) {
+        this.department = department;
+    }
+    public void setSalary(double salary) {
+        if (salary > 0) {
+            this.salary = salary;
+        }
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    public String getName() {
+        return name;
+    }
+    public Departments getDepartment() {
+        return department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+
+        Employee employee = (Employee) o;
+
+        return department.equals(employee.department);
+    }
+
     public static void main (String [] args) {
-        int [] array = new int[10];
-        int max = array[0];
-        int sum = 0;
-        int amount = 0;
-        boolean bool = false;
-
+        Employee [] employees = new Employee[5];
         Scanner scanner = new Scanner(System.in);
-        System.out.println("input 10 integer digits");
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] = scanner.nextInt();
+        System.out.println("input info about employees");
+        for (int i = 1; i <= employees.length; i++) {
+            employees[i] = new Employee();
+            System.out.println("Name: " + i);
+            employees[i].setName(scanner.nextLine());
+
+            System.out.println("Department: " + i);
+            employees[i].setDepartment(Departments.valueOf(scanner.nextLine().toUpperCase()));
+
+            System.out.println("Salary: " + i);
+            employees[i].setSalary(Double.valueOf(scanner.nextLine()));
         }
 
-        int p = 0;
-        int n = 0;
-        for (int i = 0; i < array.length; i++) {
-            if ( array[i] > max) {
-                max = array[i];
-            }
+        System.out.println("please, enter department that you need");
+        Departments dep = Departments.valueOf(scanner.nextLine().toUpperCase());
 
-            if (array[i] > 0) {
-                sum += array[i];
-                p++;
-            }
-
-            if (array[i] < 0) {
-                n++;
-            }
-
-            if (array[i] != 0){
-                amount += array[i];
-            }
-
-            if (n < p) {
-                bool = true;
-            }
-            if (n > p) {
-                bool = false;
+        for (int i = 0; i < employees.length; i++) {
+            if (dep == employees[i].department) {
+                System.out.print(employees[i].getName() + " ");
+                System.out.println(employees[i].getSalary());
             }
         }
-        System.out.println("the biggest of numbers = " + max);
-        System.out.println("the sum of positive numbers in the array = " + sum);
-        System.out.println("the amount of negative and positive numbers in the array = " + amount);
-        System.out.println("are positive ones more than negative? \n" + bool);
+
+        System.out.println();
+
+        for(int i = 1; i < employees.length; ++i) {
+            for (int j = 0; j < employees.length - i; j++) {
+                if (employees[j].getSalary() < employees[j + 1].getSalary()) {
+                    double temp = employees[j].getSalary();
+                    employees[j].setSalary(employees[j + 1].getSalary());
+                    employees[j + 1].setSalary(temp);
+                }
+            }
+        }
+
+        System.out.println("sorted by salary\n");
+        for (int i = 0; i < employees.length; i++) {
+            System.out.print(employees[i].getName() + " ");
+            System.out.print(employees[i].getDepartment() + " ");
+            System.out.print(employees[i].getSalary() + "\n");
+        }
+    }
+}
+enum Departments {
+    ADMINISTRATION, ACCOUNTING, MARKETING, PRODUCTION, SALES;
+
+    @Override
+    public String toString() {
+        String lowercase = name().toLowerCase(java.util.Locale.US);
+        return lowercase;
     }
 }
